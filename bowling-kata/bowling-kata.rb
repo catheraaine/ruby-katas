b154c15c9c3865ff4da2db080b5396d6475237bf
catheraaine <catheraaine@gmail.com>
2016-02-02 15:42:47 -0500

    Feat: Initial Commit for Bowling + Game of Life   Create .rb and RSpec files for Bowling and Game of Life Katas

---
+++ bowling-kata/bowling-kata.rb
@@ -0,0 +1,40 @@
+
+# SCORING
+# If less than 10 pins are knocked down, the score is the sum of the pins.
+# If 10 pins are knocked down on the first try of a frame, this is a strike.
+#   The score of a strike is equal to 10, plus the sum of the next two throws.
+# If 10 pins are knocked down on the second try of a frame, this is a spare.
+#   The score of a spare is equal to 10, plus the score of the next throw.
+
+# REQUIREMENTS
+# Write a class named “Game” that has two methods
+# roll(pins : int) is called each time the player rolls a ball.
+# The argument is the number of pins knocked down.
+# score() : int is called only at the very end of the game.
+# It returns the total score for that game.
+
+# => A Game is a Class
+# => A Game has 10 Frames
+# => A Frame has 1 or two rolls.
+# => The tenth frame has two or three rolls.
+#    It is different from all the other frames.
+# => The score function must iterate through all
+#    the frames, and calculate all their scores.
+# => The score for a spare or a strike depends on the frame’s successor.
+
+#Class Frame
+
+#end
+
+
+Class Game
+  attr_reader :score
+
+  def initialize
+    @score = 0
+  end
+
+
+
+
+end
