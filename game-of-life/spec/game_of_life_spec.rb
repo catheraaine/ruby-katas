3cc3b92abe87763705829c5bf7b8dea4e5df2b72
catheraaine <catheraaine@gmail.com>
2016-02-15 13:56:00 -0500

    Test: Add four empty tests to spec files.

--- game-of-life/spec/game_of_life_spec.rb
+++ game-of-life/spec/game_of_life_spec.rb
@@ -1,1 +1,33 @@
-require '../game-of-life'
+require_relative '../lib/gol-mark-i'
+
+
+describe Cycle do
+  context "when a live cell" do
+
+    context "has fewer than two live neighbor cells" do
+      it "becomes a dead cell" do
+        # When cell = true &
+      end
+    end
+
+    context "when a live cell has greater than two live neighbor cells" do
+      it "becomes a dead cell" do
+
+      end
+    end
+
+    context "when a live cell has exactly two or three live neighbor cells" do
+      it "remains a live cell" do
+
+      end
+    end
+
+  end
+
+  context "when a dead cell has exactly three live neighbor cells" do
+    it "becomes a live cell" do
+
+    end
+  end
+
+end
