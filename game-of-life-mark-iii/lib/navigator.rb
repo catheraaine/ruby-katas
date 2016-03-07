4b983b8023f096a6a185205bb8414e3c6a84c248
catheraaine <catheraaine@gmail.com>
2016-03-07 13:09:36 -0500

    :art: Refactor: Clean up tests with 'subject'

--- game-of-life-mark-iii/lib/navigator.rb
+++ game-of-life-mark-iii/lib/navigator.rb
@@ -2,22 +2,38 @@
 class Navigator
   attr_reader :cells
 
-  def initialize(grid)
+  def initialize(height, width, grid)
     @cells = grid
+    @width = width
+    @height = height
+    @newcells = Array.new(height){Array.new(width)}
   end
 
   def cells
     @cells
   end
 
-  def countNeighbors
+  def makeCells
     cells.each_with_index do |row, y|
       row.each_with_index do |cell, x|
-        puts "#{x}, #{y}"
-        puts "The cell value is #{cell}." 
+        newcell = Cell.new
+        if cell == 1
+          newcell.giveLife
+        end
+        newcells[y][x] = newcell
       end
     end
   end
+
+  # def countNeighbors
+  #   newcells.each_with_index do |row, y|
+  #     row.each_with_index do |cell, x|
+  #       if cell.alive?
+  #
+  #       end
+  #     end
+  # end
+
 
   # take the grid
   # tell it the number of live neighbors for each location
--- game-of-life-mark-iii/spec/nav-spec.rb
+++ game-of-life-mark-iii/spec/nav-spec.rb
@@ -1,13 +1,19 @@
 require_relative '../lib/navigator'
 
 describe Navigator do
+  subject(:nav) { Navigator.new(3, 3, [[0,0,0],[0,1,0],[0,0,0]]) }
+
   context "when the Navigator starts" do
     it "takes a two dimetional array" do
-      nav = Navigator.new([[0,1],[1,0]])
-      expect(nav.cells).to eq([[0,1],[1,0]])
+      expect(nav.cells).to eq([[0,0,0],[0,1,0],[0,0,0]])
     end
   end
 
-  
+  context "when given a grid of cells" do
+    it "counts the living neighbors for each cell" do
+
+    end
+
+  end
 
 end
