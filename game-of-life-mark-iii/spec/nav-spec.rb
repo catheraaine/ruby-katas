ab6595f286f7b3c72af469a7d89c2f1fbd798254
catheraaine <catheraaine@gmail.com>
2016-03-08 16:29:32 -0500

    Feat: Mark III Nav spec - all tests pass

--- game-of-life-mark-iii/spec/nav-spec.rb
+++ game-of-life-mark-iii/spec/nav-spec.rb
@@ -11,7 +11,32 @@ describe Navigator do
     end
   end
 
-  context "when given a grid of cells" do
+  context "when given a grid of data" do
+    it "creates a cell object for each cell" do
+      height = 3
+      width = 3
+      grid = [[1,0,1],[0,1,0],[1,0,0]]
+      nav = Navigator.new(height, width, grid)
+      expect(nav.newcells[1][1]).to be_a(Cell)
+    end
+
+    it "assigns a living or dead state to a cell" do
+    height = 3
+    width = 3
+    grid = [[1,0,1],[0,1,0],[1,0,0]]
+    nav = Navigator.new(height, width, grid)
+    expect(nav.newcells[1][1].alive?).to be true
+    end
+
+    it "assigns a location to each cell object" do
+      height = 3
+      width = 3
+      grid = [[1,0,1],[0,1,0],[1,0,0]]
+      nav = Navigator.new(height, width, grid)
+      expect(nav.newcells[1][0].x).to eq(1)
+      expect(nav.newcells[1][0].y).to eq(0)
+    end
+
     it "counts the living neighbors for each cell" do
       height = 3
       width = 3
